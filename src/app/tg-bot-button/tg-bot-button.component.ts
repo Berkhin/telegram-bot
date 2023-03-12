a9d12456a11adfdd9b139255f99d21f25b911891
Berkin <evgeniy.berkin@shieldfc.com>
2023-03-12 17:46:30 +0200

    refactored

--- src/app/tg-bot-button/tg-bot-button.component.ts
+++ src/app/tg-bot-button/tg-bot-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tg-bot-button',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./tg-bot-button.component.scss']
 })
 export class TgBotButtonComponent {
-  
+  @Input() textBtn: string = '';
+  constructor(){
+
+  }
 }
