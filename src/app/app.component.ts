4ce27c656884c794a91d597a4f34a68d3f7a8ce4
Berkin <evgeniy.berkin@shieldfc.com>
2023-03-12 09:55:08 +0200

    added window object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'telegram-bot-web';
+    tg = window.Telegram?.WebApp;
 
   onClose(){
-    
+    this.tg.close();
   }
 }
